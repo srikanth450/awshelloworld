dba85b1e3d385db0d83aca22fe24f17c29376418
srikanth450 <srikanth.bammidi@gmail.com>
2020-01-11 23:06:33 +0530

    just changed print stmt

--- src/main/java/awshelloworld.java
+++ src/main/java/awshelloworld.java
@@ -1,6 +1,6 @@
 public class awshelloworld {
     public static void main(String args[]) {
 
-        System.out.println("Test message from Java...!");
+        System.out.println("Test message from Javaaaaa...!");
     }
 }
