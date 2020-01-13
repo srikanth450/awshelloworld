d3ffadbeb414f8be0f09c2253a8bc7f8ecdaba94
srikanth450 <srikanth.bammidi@gmail.com>
2020-01-13 15:54:23 +0530

    added display

--- src/main/java/awshelloworld.java
+++ src/main/java/awshelloworld.java
@@ -2,5 +2,6 @@ public class awshelloworld {
     public static void main(String args[]) {
 
         System.out.println("Test message from Javaaaaa...!");
+        System.out.println("Test message from Javaaaaa srikanth bammidi...!");
     }
 }
