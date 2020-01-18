42bcb3ddda47f871a6c05bcfb80f6f110299de9e
srikanth450 <srikanth.bammidi@gmail.com>
2020-01-19 01:15:04 +0530

    print statment changed

--- src/main/java/awshelloworld.java
+++ src/main/java/awshelloworld.java
@@ -1,7 +1,7 @@
 public class awshelloworld {
     public static void main(String args[]) {
 
-        System.out.println("Test message from Javaaaaa...!");
+        System.out.println("Test message from Javaaaaahe he he...!");
         System.out.println("Test message from Javaaaaa srikanth bammidi1234567...!");
     }
 }
