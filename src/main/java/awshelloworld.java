eb47822ed7a82cb241fcfef660c3a2ef0d2b9e20
srikanth450 <srikanth.bammidi@gmail.com>
2020-01-19 02:18:01 +0530

    removed ; for print statment

--- src/main/java/awshelloworld.java
+++ src/main/java/awshelloworld.java
@@ -2,6 +2,6 @@ public class awshelloworld {
     public static void main(String args[]) {
 
         System.out.println("Test message from Javaaaaahe he he1234...!");
-        System.out.println("Test message from Javaaaaa srikanth bammidi1234567...!");
+        System.out.println("Test message from Javaaaaa srikanth bammidi1234567...!")
     }
 }
