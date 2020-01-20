33762b0f090dc01647779daa918f84ecad152141
srikanth450 <srikanth.bammidi@gmail.com>
2020-01-20 17:47:16 +0530

    changed print statment to fail bind

--- src/main/java/awshelloworld.java
+++ src/main/java/awshelloworld.java
@@ -2,6 +2,6 @@ public class awshelloworld {
     public static void main(String args[]) {
 
         System.out.println("Test message from Javaaaaahe he he123456...!");
-        System.out.println("Test message from Javaaaaa srikanth bammidi1234567...!");
+        System.out.println("Test message from Javaaaaa srikanth bammidi1234567...!")
     }
 }
